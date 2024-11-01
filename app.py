342fc5570a15bd2468bcf8bc12e3d9cdac284564
kevinSolano <kevinstivensolanomejia@gmail.com>
2024-10-31 19:09:09 -0500

    integrados todos los botones con sus señales

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from PyQt5 import uic
 import sys
 from PyQt5.QtGui import QPixmap
 
+from calculator_ang import receptionDate
+
 class Principal(pyqt.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -10,9 +12,22 @@ class Principal(pyqt.QMainWindow):
     
     def initGUI(self):
         uic.loadUi('canon_interfaz.ui',self)
-        self.manual_menu.clicked.connect(lambda: self.isclic())  
-        self.automatic_menu.clicked.connect(lambda: self.isclic1()) 
-        self.ajustar_auto.clicked.connect(lambda: self.ajustar())
+
+        self.manual_menu.clicked.connect(lambda: self.isclicManual())  
+        self.automatic_menu.clicked.connect(lambda: self.isclicAuto()) 
+        self.ajustar_auto.clicked.connect(lambda: self.ajustarAuto())
+
+        #valores del manual cambiados
+        self.angulo_base.valueChanged.connect(lambda: self.ajustarManual())
+        self.angulo_canon.valueChanged.connect(lambda: self.ajustarManual())
+
+        #disparar:
+
+        self.disparar_auto.clicked.connect(lambda: self.disparar())
+        self.disparar_manual.clicked.connect(lambda: self.disparar())
+
+        #recargar:
+        self.recarga.clicked.connect(lambda: self.recargar())
 
         pixmap = QPixmap("images\logodragon.png")
         self.image.setPixmap(pixmap)
@@ -23,15 +38,30 @@ class Principal(pyqt.QMainWindow):
         
         self.show()
     
-    def ajustar(self):
-        print('ajustado')
+    def disparar(self):
+        receptionDate(0,0,'disp')
 
-    def isclic(self):
+    def recargar(self):
+        receptionDate(0,0,'rec')
+
+    def ajustarAuto(self):
+        base_ang = self.barra_base_auto.value()
+        distancia = self.distancia_auto.value()
+        receptionDate(base_ang,distancia,'auto')
+        self.recargar()
+
+    def ajustarManual(self):
+        base_ang = self.angulo_base.value()
+        canon_ang = self.angulo_canon.value()
+        receptionDate(base_ang,canon_ang,'manual')
+
+
+    def isclicManual(self):
         self.manual_menu.setStyleSheet("background-color: rgb(03, 08, 17);font: 700 9pt 'Tahoma';color: rgb(249, 255, 239);border-radius: 20px;")
         self.automatic_menu.setStyleSheet("background-color: rgb(23, 28, 47);font: 700 9pt 'Tahoma';color: rgb(249, 255, 239);border-radius: 20px;")
+        self.ajustarManual()
 
-
-    def isclic1(self):
+    def isclicAuto(self):
         self.manual_menu.setStyleSheet("background-color: rgb(23, 28, 47);font: 700 9pt 'Tahoma';color: rgb(249, 255, 239);border-radius: 20px;")
         self.automatic_menu.setStyleSheet("background-color: rgb(03, 08, 27);font: 700 9pt 'Tahoma';color: rgb(249, 255, 239);border-radius: 20px;")        
         
---
+++ calculator_ang.py
@@ -0,0 +1,10 @@
+
+
+def receptionDate(base:int,valor2:int,mode:str):
+    if mode=='disp':
+        print('Disparar!!!!')
+    elif mode=='rec':
+        print('recargando')
+    else:
+        print(base,valor2,mode)
+
