a47ba9be4c9a9ad3b886356627aaa133fce14e13
kevinSolano <kevinstivensolanomejia@gmail.com>
2024-11-06 17:18:51 -0500

    mejora de los errores del connect2bt

--- connect2bt.py
+++ connect2bt.py
@@ -1,26 +1,33 @@
 import serial
 import time
 
-
+connection = None
 
 def conectar():
     global connection
     try:
-        connection = serial.Serial(port='COM8',baudrate= 115200)
+        connection = serial.Serial(port='COM7', baudrate=115200, timeout=1)
         time.sleep(2)
-        
-        mess = ('conección hecha  :)')
-    except:
-        
-        mess = ('no se puedo conectar :(')
+        mess = 'Conexión hecha :)'
+    except serial.SerialException as e:
+        mess = f'No se pudo conectar :( - Error: {e}'
     return mess
-    
+
 def envio2esp(mensaje):
     try:
-        connection.write(mensaje.encode())
-        mess = ('envio correcto')
-    except:
-        
-        mess = ('algo a fallado')
-
+        if connection and connection.is_open:
+            connection.write(mensaje.encode())
+            mess = 'Envío correcto'
+        else:
+            mess = 'Conexión no está abierta'
+    except serial.SerialException as e:
+        mess = f'Fallo en el envío - Error: {e}'
+        print(e)
+    except AttributeError:
+        mess = 'Conexión no establecida'
     return mess
+
+def cerrar_conexion():
+    global connection
+    if connection and connection.is_open:
+        connection.close()
